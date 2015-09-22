6dcbafe2e0a939b1dd933b6736af2aafb3e46fb0
John Orme <jorme@gcdtech.com>
2015-09-22 16:43:03 +0100

    switching namespacing to rhubarb for js

--- src/Presenters/BackgroundTaskFullFocusViewBridge.js
+++ src/Presenters/BackgroundTaskFullFocusViewBridge.js
@@ -1,9 +1,9 @@
 var bridge = function (presenterPath)
 {
-    window.gcd.core.mvp.viewBridgeClasses.BackgroundTaskViewBridge.apply( this, arguments );
+    window.rhubarb.viewBridgeClasses.BackgroundTaskViewBridge.apply( this, arguments );
 };
 
-bridge.prototype = new window.gcd.core.mvp.viewBridgeClasses.BackgroundTaskViewBridge();
+bridge.prototype = new window.rhubarb.viewBridgeClasses.BackgroundTaskViewBridge();
 bridge.prototype.constructor = bridge;
 
 bridge.prototype.onComplete = function()
@@ -11,4 +11,4 @@ bridge.prototype.onComplete = function()
     this.raisePostBackEvent("TaskComplete", this.model.BackgroundTaskStatusID );
 };
 
-window.gcd.core.mvp.viewBridgeClasses.BackgroundTaskFullFocusViewBridge = bridge;
+window.rhubarb.viewBridgeClasses.BackgroundTaskFullFocusViewBridge = bridge;
